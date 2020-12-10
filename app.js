4a3142c2427dfa0455f22e3596cc9fa63e971706
RizkyMaulita <rizky.maulita@gmail.com>
2020-12-10 20:48:11 +0700

    add socket for adduser

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const PORT = process.env.PORT || 3000
 
+const users = [
+  {
+    username: 'Litha'
+  }
+]
 io.on('connect', function(socket){
   console.log(`Socket io client connected`);
+  socket.emit('init', users)
+  socket.on('addUser', (payload)=>{
+    users.push({ username: payload })
+    io.emit('CreateUser', payload)  //jalan di semua client
+  })
 })
 
 server.listen(PORT, ()=>{
