342b89ee14cc45bc4614aa4b540ce7766bb7c73a
hndrbudisantoso@gmail.com <hndrbudisantoso@gmail.com>
2020-12-11 12:59:15 +0700

    fixing refresh lobby unreloaded room card

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const users = [
   }
 ]
 const rooms = []
+let room = {}
 // nanti hasilnya kurang lebih kek gini --> [ { name: 'litha-room', maxPlayer: '2', admin: 'litha', users: [] } ]
 
 io.on('connect', function(socket){
   console.log(`Socket io client connected`);
-  socket.emit('init', { users, rooms })
+  socket.emit('init', { users, rooms, room })
   socket.on('addUser', payload => {
     users.push({ username: payload })
     io.emit('CreateUser', payload)  //jalan di semua client
@@ -34,12 +35,14 @@ io.on('connect', function(socket){
     socket.join(payload.roomName, function(){
       // console.log(socket.rooms, '<<<rooms');
       let roomIndex = rooms.findIndex((i) => i.name == payload.roomName)
+      
       if(!rooms[roomIndex].users.includes(payload.username) && 
-          rooms[roomIndex].users.length < +rooms[roomIndex].maxPlayer) {
+          rooms[roomIndex].users.length <= +rooms[roomIndex].maxPlayer) {
         rooms[roomIndex].users.push(payload.username)
       }
       console.log(rooms[roomIndex]);
-      io.sockets.in(payload.roomName).emit('roomDetail', rooms[roomIndex])
+      room = rooms[roomIndex]
+      io.sockets.in(payload.roomName).emit('getRoom', room)
     })
   })
 })
